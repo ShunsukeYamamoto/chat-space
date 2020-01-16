c70f7c90ea60e7d7afb4bd53494ea00998774dc2
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-01-16 17:50:40 +0900

    design_message_path

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,19 @@ class Group < ApplicationRecord
   validates :name, presence: true, null: false
   has_many :users, through: :group_users
   has_many :group_users
+  has_many :messages
+
+def show_last_message
+  last_message = messages.last
+  if last_message.present?
+    if last_message.content.present?
+      last_message.content
+    else
+      "画像が投稿されています"
+    end
+  else
+    "まだ投稿がありません"
+  end
+end
+
 end
---
+++ app/models/message.rb
@@ -0,0 +1,7 @@
+class Message < ApplicationRecord
+  validates :content,presence: true,unless: :image?
+
+  belongs_to :group
+  belongs_to :user
+  mount_uploader :image, ImageUploader
+end
